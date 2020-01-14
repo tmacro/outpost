0cd65fa193a269934672991bfe7c3112b2fec243
Taylor McKinnon <mail@tmacs.space>
2020-01-13 22:36:11 -0800

    cloud_init: rename output variable

--- terraform/modules/cloud-init/output.tf
+++ terraform/modules/cloud-init/output.tf
@@ -1,4 +1,4 @@
-output "cloud-init" {
-  value = data.template_cloudinit_config.config.rendered
+output "cloud_init" {
+  value     = data.template_cloudinit_config.config.rendered
   sensitive = true
 }
